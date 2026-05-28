479036319e253e982ddb214c7e9a5b06e6a5d70f
Sauls16 <saulstgo04@gmail.com>
2026-05-27 21:08:26 -0600

    Ligeros cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Inventario CC</title>
+    <title> Centro de Computo</title>
     <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
 </head>
 <body>
